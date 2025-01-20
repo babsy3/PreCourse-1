787ccb5802fa1af06cc9510d4669337b8ccb0a04
Babsy Vellera Babu <babu.b@northeastern.edu>
2025-01-20 16:45:37 -0500

    Exercise 3

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,9 +1,17 @@
+#Implement Singly Linked List.
+# Time Complexity : O(n) for all operations
+# Space Complexity : O(n) 
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this :
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,6 +25,12 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+
+        #Traverse to end of list
+        curr = self.head
+        while curr.next is not None:
+            curr = curr.next
+        curr.next = ListNode(data,None)
         
     def find(self, key):
         """
@@ -24,9 +38,28 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        if self.head.data == key:
+            return self.head
+        curr = self.head
+        while curr.next is not None:
+            if curr.data == key:
+                return curr
+            curr = curr.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+
+        curr = self.head
+        prev = None
+        while curr is not None:
+            if curr.data == key:
+                if prev == None:
+                    self.head = curr.next
+                else:
+                    prev.next = curr.next
+            prev = curr
+            curr = curr.next
